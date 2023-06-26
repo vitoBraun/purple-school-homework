88727634523fb5f738a4c89c6685d4b7dabb752a
Vitalii Braun <vitossvx@gmail.com>
2023-06-26 21:46:06 +0300

    Add check for existing user

--- 8-2-authorization-api/src/services/user.service.js
+++ 8-2-authorization-api/src/services/user.service.js
@@ -1,6 +1,17 @@
 import prisma from "./prisma.service.js";
 
 export const registerUserService = async (name, email, password) => {
+  const exisitngUser = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+  if (exisitngUser) {
+    return new Promise((resolve, reject) =>
+      reject(`User ${email} already exists`)
+    );
+  }
+
   return prisma.user.create({
     data: {
       name,
